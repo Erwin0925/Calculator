dc52d8b1807fd35d25dc8fb91807dd6b576ee39a
Erwin <108355646+Erwin0925@users.noreply.github.com>
2024-12-28 03:09:37 +0800

    History to review calculated numbers - Replace List with Stack so that the latest data is always on top

---
+++ Calculator/Function/History.cs
@@ -0,0 +1,39 @@
+﻿namespace Calculator.Function
+{
+    internal class History
+    {
+        private static History? instance;
+        private Stack<string> historyStack;
+
+        private History()
+        {
+            historyStack = new Stack<string>();
+        }
+
+        public static History Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new History();
+                }
+                return instance;
+            }
+        }
+
+        public void StoreHistory(string record)
+        {
+            historyStack.Push(record);
+        }
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("Calculation History:");
+            foreach (var record in historyStack)
+            {
+                Console.WriteLine(record);
+            }
+        }
+    }
+}
